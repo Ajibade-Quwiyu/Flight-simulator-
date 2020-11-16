0d6c458238de22dc3ddaf51194983f7b34b28a23
Quwiyu <68051831+Ajibade-Quwiyu@users.noreply.github.com>
2020-11-16 09:51:32 +0100

    Add files via upload

---
+++ PlaneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public float turnSpeed = 10000;
+
+    public Transform TopRotor;
+    public Transform TailRotor;
+
+    Rigidbody Plane;
+    public Vector3 UpwardForce = Vector3.zero;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Plane = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        TopRotor.transform.Rotate(0, turnSpeed, 0);
+        TailRotor.transform.Rotate(turnSpeed, 0, 0);
+    }
+}
